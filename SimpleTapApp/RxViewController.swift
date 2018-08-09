f81947fc93b7943ed18235ff4119937fea55b5b2
ryo_takahashi <ryo-takahashi@tokubai.co.jp>
2018-08-09 23:14:30 +0900

    swiftlint autocorrect

--- SimpleTapApp/RxViewController.swift
+++ SimpleTapApp/RxViewController.swift
@@ -3,22 +3,22 @@ import RxSwift
 import RxCocoa
 
 class RxViewController: UIViewController {
-    
+
     @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var countUpButton: UIButton!
     @IBOutlet weak var countDownButton: UIButton!
     @IBOutlet weak var countResetButton: UIButton!
-    
+
     private let disposeBag = DisposeBag()
-    
+
     let viewModel = RxViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViewController()
         setupViewModel()
     }
-    
+
     private func setupViewController() {
         countUpButton.rx.tap
             .subscribe(onNext: { [weak self] in
@@ -31,36 +31,36 @@ class RxViewController: UIViewController {
                 self?.viewModel.decrementCount()
             })
             .disposed(by: disposeBag)
-        
+
         countResetButton.rx.tap
             .subscribe(onNext: { [weak self] in
                 self?.viewModel.resetCount()
             })
             .disposed(by: disposeBag)
     }
-    
+
     private func setupViewModel() {
         viewModel.countRelay
             .map { return "Rxパターン: \($0)"}
             .bind(to: countLabel.rx.text)
             .disposed(by: disposeBag)
     }
-    
+
 }
 
 class RxViewModel {
     let countRelay = BehaviorRelay<Int>(value: 0)
-    
+
     func incrementCount() {
         let count = countRelay.value
         countRelay.accept(count + 1)
     }
-    
+
     func decrementCount() {
         let count = countRelay.value
         countRelay.accept(count - 1)
     }
-    
+
     func resetCount() {
         countRelay.accept(0)
     }
